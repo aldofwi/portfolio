e4ab5e6fd1b670af418af1fc82af166b9a88b49a
Aldo FWI <aldo.fwi@gmail.com>
2022-12-21 19:00:56 -0400

    Newsletter Update

---
+++ src/Components/MailChimpForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import MailchimpSubscribe from 'react-mailchimp-subscribe'
+import { NewsLetter } from './NewsLetter';
+
+export const MailChimpForm = () => {
+    // Fill the fields in .env file from MailChimp Account.
+    const postURL = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+
+    return (
+        <>
+            <MailchimpSubscribe 
+                url={postURL}
+                render={({ subscribe, status, message }) => (
+                    <NewsLetter 
+                        status={status}
+                        message={message}
+                        onValidated={formData => subscribe(formData)}
+                    />
+                )}
+            />
+        </>
+    )
+}
